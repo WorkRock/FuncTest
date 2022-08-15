db63c9c7d1286c191667db4dfba111216c252c2a
hard-puncher <xpsmslrwh@naver.com>
2022-08-15 15:42:41 +0900

    FuncTest_Ingame_05

--- FuncTest/Assets/Scripts/IG_BtnManager.cs
+++ FuncTest/Assets/Scripts/IG_BtnManager.cs
@@ -7,6 +7,20 @@ using UnityEngine.SceneManagement;
 public class IG_BtnManager : MonoBehaviour
 {
     [Space(15f)]
+    [Header("Player")]
+    public Text Player_NowHP_T;
+    public Text Player_TotalHP_T;
+
+    public Text Player_ATK_T;
+    public Text Player_ShieldCT_T;
+
+    public float Player_NowHP;
+    public float Player_TotalHP;
+
+    public float Player_ATK;
+    public float Player_ShieldCT;
+
+    [Space(15f)]
     [Header("Dragon")]
     public Text Dragon_TotalHP_Text;
     public Text Dragon_NowHP_Text;
@@ -98,7 +112,22 @@ public class IG_BtnManager : MonoBehaviour
     {
         Stage = 1;
         Stage_Text.text = $"Stage : {Stage}";
-        
+
+        // #1 hp초기화
+        Player_TotalHP = ScoreManager.GetPlayerHP();
+        Player_NowHP = Player_TotalHP;
+
+        Player_TotalHP_T.text = $"TotalHp : {Player_TotalHP}";
+        Player_NowHP_T.text = $"NowHp : {Player_NowHP}";
+
+        // #2 공격력
+        Player_ATK = ScoreManager.GetPlayerTotalAtk();
+        Player_ATK_T.text = $"ATK : {Player_ATK}";
+
+        // #3 쉴드 쿨타임
+        Player_ShieldCT = ScoreManager.GetShieldCT();
+        Player_ShieldCT_T.text = $"Shield CT : {Player_ShieldCT}";
+
 
         // #4-1 드래곤 HP 초기화
         Dragon_TotalHP = Dragon_BasicDefault_HP;
@@ -111,7 +140,11 @@ public class IG_BtnManager : MonoBehaviour
         ComObj_ATK = BasicDefault_ComObj_ATK;
         ComObj_Speed = BasicDefault_ComObj_Speed;
         ComObj_Delay = BasicDefault_ComObj_Delay;
-    }
+
+        ComObj_Delay_Text.text = $"ComObj_Delay : {ComObj_Delay}";
+        ComObj_ATK_Text.text = $"ComObj_ATK : {ComObj_ATK}";
+        ComObj_Speed_Text.text = $"ComObj_Speed : {ComObj_Speed}";
+}
 
     void Update()
     {
@@ -120,7 +153,7 @@ public class IG_BtnManager : MonoBehaviour
 
     public void Dragon_Hit()
     {
-        //Dragon_NowHP -= Player_ATK;
+        Dragon_NowHP -= Player_ATK;
         Dragon_NowHP_Text.text = $"NowHP : {Dragon_NowHP}";
 
         if(Dragon_NowHP <= 0)
@@ -129,7 +162,7 @@ public class IG_BtnManager : MonoBehaviour
             Stage_Text.text = $"Stage : {Stage}";
 
             // #4 드래곤 체력 증가
-            Dragon_TotalHP += ScoreManager.totalFloatFormula(Stage, Dragon_BasicPlus_HP, Dragon_BasicCorStage_HP,
+            Dragon_TotalHP = Dragon_BasicDefault_HP + ScoreManager.totalFloatFormula(Stage - 1, Dragon_BasicPlus_HP, Dragon_BasicCorStage_HP,
                 Dragon_EditDefault_HP, Dragon_EditPlus_HP, Dragon_EditCorStage_HP);
 
             Dragon_NowHP = Dragon_TotalHP;
@@ -139,26 +172,29 @@ public class IG_BtnManager : MonoBehaviour
             Dragon_NowHP_Text.text = $"NowHp : {Dragon_NowHP}";
 
             // # 5 오브젝트 공격력, 스피드, 딜레이 증감
-            ComObj_ATK += ScoreManager.totalFloatFormula(Stage, BasicPlus_ComObj_ATK, BasicCorStage_ComObj_ATK,
+            ComObj_ATK = BasicDefault_ComObj_ATK + ScoreManager.totalFloatFormula(Stage - 1, BasicPlus_ComObj_ATK, BasicCorStage_ComObj_ATK,
                 EditDefault_ComObj_ATK, EditPlus_ComObj_ATK, EditCorStage_ComObj_ATK);
+            ComObj_ATK_Text.text = "ComObj_ATK : " + ComObj_ATK.ToString();
 
-            ComObj_Speed += ScoreManager.totalFloatFormula(Stage, BasicPlus_ComObj_Speed, BasicCorStage_ComObj_Speed,
+            ComObj_Speed = BasicDefault_ComObj_Speed + ScoreManager.totalFloatFormula(Stage - 1, BasicPlus_ComObj_Speed, BasicCorStage_ComObj_Speed,
                 EditDefault_ComObj_Speed, EditPlus_ComObj_Speed, EditCorStage_ComObj_Speed);
+            ComObj_Speed_Text.text = "ComObj_Speed : " + ComObj_Speed.ToString();
 
-            ComObj_Delay += ScoreManager.totalFloatFormula(Stage, BasicPlus_ComObj_Delay, BasicCorStage_ComObj_Delay,
+            ComObj_Delay = BasicDefault_ComObj_Delay + ScoreManager.totalFloatFormula(Stage - 1, BasicPlus_ComObj_Delay, BasicCorStage_ComObj_Delay,
                 EditDefault_ComObj_Delay, EditPlus_ComObj_Delay, EditCorStage_ComObj_Delay);
-
+            ComObj_Delay_Text.text = "ComObj_Delay : " + ComObj_Delay.ToString();
         }
     }
 
     public void Player_Hit()
-    {/*
+    {
         Player_NowHP -= ComObj_ATK;
-        Player_NowHP_Text.text = $"NowHp : {Player_NowHP}";
+        Player_NowHP_T.text = $"NowHp : {Player_NowHP}";
 
         if (Player_NowHP <= 0)
+        {
+            ScoreManager.SetBStage(Stage);
             SceneManager.LoadScene("Result");
-        */
+        }
     }
-
 }
